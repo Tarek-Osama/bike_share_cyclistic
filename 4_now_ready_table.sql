c6c253efca224bfb9b0d4a667d0af4be55e7a083
Tarek-Osama <144431998+Tarek-Osama@users.noreply.github.com>
2023-09-09 10:09:53 +0300

    4_now_ready_table.sql

--- 4_now_ready_table.sql
+++ 4_now_ready_table.sql
@@ -1,8 +1,9 @@
 /*Bike-sharing company "Cyclistic" Case Study: 
- Changing data type for some columns and Creating final table to analysis. 
+ Changing data type for some columns and Creating a final table for analysis. 
 */
 
-
+--before running the query, Note: change query sittings for destination to "Set a destination table for query results".
+--to create a permanent table named "now_ready".
 
 SELECT
   ride_id,
@@ -26,5 +27,20 @@ SELECT
 FROM `cyc-trips-2022.data_2022.ready_to_analysis`
 
 
+-- Updating day_of_week to days names.
 
-
+UPDATE
+  `cyc-trips-2022.data_2022.now_ready`
+SET
+  day_of_week =
+  CASE
+    WHEN day_of_week = '1' THEN 'Sunday'
+    WHEN day_of_week = '2' THEN 'Monday'
+    WHEN day_of_week = '3' THEN 'Tuesday'
+    WHEN day_of_week = '4' THEN 'Wednesday'
+    WHEN day_of_week = '5' THEN 'Thursday'
+    WHEN day_of_week = '6' THEN 'Friday'
+    WHEN day_of_week = '7' THEN 'Saturday'
+END
+WHERE
+  day_of_week IN ('1', '2', '3', '4', '5', '6', '7')
