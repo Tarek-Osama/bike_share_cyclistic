d800f490c7222e3145a0e49db6469131735bc9d4
Tarek-Osama <144431998+Tarek-Osama@users.noreply.github.com>
2023-09-09 09:15:48 +0300

    remove_lat_long_data.sql

--- remove_lat_long_data.sql
+++ remove_lat_long_data.sql
@@ -1,1 +1,20 @@
+/*
+Removing lat and long columns because they will not be used in the analysis.
+*/
 
+
+
+
+SELECT
+  ride_id,
+  rideable_type,
+  started_at,
+  ended_at,
+  start_station_name,
+  start_station_id,
+  end_station_name,
+  end_station_id,
+  member_casual,
+  day_of_week
+FROM
+  `cyc-trips-2022.data_2022.all_yr_trips`
